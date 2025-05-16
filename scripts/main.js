a2c5b47ce5010848cb9c11ea2bc0836718d0e5e8
HaraldIsHere <duplouis.harald@gmail.com>
2025-05-16 11:56:19 +0200

    Init

---
+++ scripts/main.js
@@ -0,0 +1,80 @@
+// ==============================
+// 🌱 Sélection des éléments
+// ==============================
+
+// Inputs
+const inputName = document.querySelector(`.article-name`);
+const inputPrice = document.querySelector(`.article-price`);
+const inputQuantity = document.querySelector(`.article-quantity`);
+
+// Button
+const addButton = document.querySelector(`.add-button`);
+
+// Display
+const displayedList = document.querySelector(`.displayed-list`);
+const displayTotalPrice = document.querySelector(`.display-total-price`)
+// ==============================
+// 🧠 Variables globales
+// ==============================
+const commandesListe = [];
+// ==============================
+// 🎊 Fonctionnalités
+// ==============================
+class Commande {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    commandePrice() {
+        this.totalprice = this.quantity * this.price;
+        return this.quantity * this.price;
+    }
+
+    displayCommande() {
+        return (`Product: ${this.name} | Price: ${this.price} | Quantity: ${this.quantity} | Total Price: ${this.commandePrice()}`);
+    }
+}
+
+function resetInputs() {
+    inputName.value = ``;
+    inputPrice.value = ``;
+    inputQuantity.value = ``;
+}
+
+function addCommandeToList() {
+    commandesListe.push(new Commande(inputName.value, inputPrice.value, inputQuantity.value));
+    resetInputs()
+}
+
+function displayCommandesList() {
+    displayedList.innerHTML = ``;
+
+    commandesListe.forEach((commande) => {
+        displayedList.innerHTML += (`<div>${commande.displayCommande()} </div>`);
+    })
+}
+
+function displayTotalCommandePrice() {
+    displayTotalPrice.innerHTML = ``;
+
+    totalPriceTemp = 0;
+    commandesListe.forEach((commande) => {
+        totalPriceTemp += commande.commandePrice();
+    })
+
+    displayTotalPrice.innerHTML += (`<div>Prix total : ${totalPriceTemp} </div>`);
+}
+
+// ==============================
+// 🧲 Événements
+// ==============================
+addButton.addEventListener(`click`, function(e) {
+    e.preventDefault();
+    addCommandeToList();
+    displayCommandesList();
+    displayTotalCommandePrice();
+    console.log(commandesListe);
+    
+})
